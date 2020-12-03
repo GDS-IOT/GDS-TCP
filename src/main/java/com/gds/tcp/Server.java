e9b675f3d650030968e193b4a39070644f74679a
Sujith <sujithramanathan@gmail.com>
2020-12-03 17:52:08 +0530

    Added byte jar

--- src/main/java/com/gds/tcp/Server.java
+++ src/main/java/com/gds/tcp/Server.java
@@ -2,6 +2,9 @@ package com.gds.tcp;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -16,49 +19,71 @@ import java.net.Socket;
  */
 public class Server {
 
-    public static void main(String []args) throws IOException, InterruptedException {
-        while(true) {
-            System.out.println("Server Started");
-            ServerSocket serverSocket = null;
-            Socket client = null;
-            try {
-                serverSocket = new ServerSocket(1045);
-                client = serverSocket.accept();
-                BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                BufferedWriter toClient = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-                String fromMsg = fromClient.readLine();
-                String toMsg = "";
-                while (true) {
-                    if (null == fromMsg)
-                        fromMsg = "null";
-                    toMsg = "Message Received :: ".concat(fromMsg);
-                    System.out.println(toMsg);
-                    toClient.write(toMsg);
-                    toClient.newLine();
-                    toClient.flush();
-                    fromMsg = fromClient.readLine();
-                    if(null == fromMsg){
-                        System.out.println("Message is null");
-                        client.close();
-                        serverSocket.close();
-                        break;
-                    }
-                }
-            } catch (IOException ie) {
-                ie.printStackTrace();
-                if (null != client) {
-                    client.close();
-                    serverSocket.close();
-                }
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                e.printStackTrace();
-                if (null != client) {
-                    client.close();
-                    serverSocket.close();
-                }
-                Thread.sleep(1000);
+    public static void viaString() {
+        System.out.println("Server Started");
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(10008);
+            Socket client =  serverSocket.accept();
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter toClient = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            String fromMsg = fromClient.readLine();
+            String toMsg = "";
+            while(null != fromMsg && !"".equals(fromMsg)) {
+                toMsg = "Message Received :: ".concat(fromMsg);
+                toClient.write(toMsg);
+                toClient.newLine();
+                toClient.flush();
+                fromMsg = fromClient.readLine();
             }
+        }catch(IOException ie) {
+            ie.printStackTrace();
+        }catch(Exception e) {
+            e.printStackTrace();
         }
     }
+
+    public static void viaByte() {
+        System.out.println("Server Started - Bytes");
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(10008);
+            Socket client =  serverSocket.accept();
+            DataInputStream fromClient = new DataInputStream(client.getInputStream());
+            DataOutputStream toClient = new DataOutputStream(client.getOutputStream());
+
+            byte []clientData = null;
+            byte []toMsg = new byte[2];
+            toMsg[0] = 2; toMsg[1] = 1;
+
+            while(true) {
+                try {
+                    clientData = new byte[fromClient.readInt()];
+                    fromClient.readFully(clientData);
+                    for(int i=0; i < clientData.length; i++) {
+                        System.out.print((int)clientData[i]+" ");
+                    }
+                    System.out.print("\nSuccessfully Received");
+                    toClient.writeInt(2);
+                    toClient.write(toMsg);
+                }catch(EOFException e) {
+                    System.out.println("\nReinitializing");
+                    client =  serverSocket.accept();
+                    fromClient = new DataInputStream(client.getInputStream());
+                    toClient = new DataOutputStream(client.getOutputStream());
+                    System.out.println("Reinitialized");
+                }
+            }
+        }catch(IOException ie) {
+            ie.printStackTrace();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String []args) {
+        viaByte();
+    }
+
 }
+
