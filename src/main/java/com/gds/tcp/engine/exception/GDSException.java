cf58881b5614e38d92eb23f935153b5901068664
Sujith <sujithramanathan@gmail.com>
2020-12-12 03:18:12 +0530

    Platform Engine Initial Draft.

---
+++ src/main/java/com/gds/tcp/engine/exception/GDSException.java
@@ -0,0 +1,13 @@
+package com.gds.tcp.engine.exception;
+
+
+/**
+ * @Sujith Ramanathan
+ */
+public class GDSException extends RuntimeException{
+
+    public GDSException(String msg){
+        super(msg);
+    }
+
+}
